89354ae2d85bca92353460ac57bc1982023407d1
Claudia Latella <claudia.latella.86@gmail.com>
2016-10-28 13:55:18 +0200

    started working on the algorithm for estimating the sufficient measurements number to compute the sensor position.

--- Experiments/23links_human/src/computeSuitSensorPosition.m
+++ Experiments/23links_human/src/computeSuitSensorPosition.m
@@ -10,6 +10,17 @@ for sIdx = 1: suit.properties.nrOfSensors
     [link, ~] = linksFromName(suit.links, sensor.attachedLink);
     A = zeros(3*len,3);
     b = zeros(3*len,1);
+    Rbig = [];
+    SigmaBig = [];
+    SigmaSens = cell(len,1);
+    eigen = zeros(3,len);
+    eigenMax = zeros(1,len);
+    SigmaDet = zeros(1,len);
+    Sigma_x_inv = eye(3); %1/0.0625
+    Sigma_A_inv = 1/0.001111 * eye(3); %from datasheet
+    Sigma_xgivena_inv = Sigma_x_inv;
+    mu_xgivena = zeros(3,1);
+    norm_Sigma_xgivena_inv = zeros(1,len);
 
     for i = 1 : len
         S1 = skewMatrix(link.meas.angularAcceleration(:,i));
@@ -31,6 +42,24 @@ for sIdx = 1: suit.properties.nrOfSensors
         G_acc_L = link.meas.acceleration(:,i);
 
         b(3*i-2:3*i) = G_acc_S - G_acc_L + [0;0;-9.81];
+        
+        Sigma_xgivena_inv = Sigma_xgivena_inv + ...
+            A(3*i-2:3*i,:)' * Sigma_A_inv * A(3*i-2:3*i,:);
+        mu_xgivena = mu_xgivena + ...
+            Sigma_xgivena_inv\(A(3*i-2:3*i,:)' *  Sigma_A_inv * b(3*i-2:3*i));
+        
+        norm_Sigma_xgivena_inv(i) = norm(inv(Sigma_xgivena_inv));
+
+        Rbig = blkdiag(Rbig, G_R_S);
+        SigmaBig = blkdiag(SigmaBig,  0.001111 * eye(3));
+        Abig = A(1:3*i,:);
+        SigmaSens{i} = pinv(Abig) * Rbig * SigmaBig * Rbig' * (pinv(Abig)');
+        eigen(:,i) = eig(SigmaSens{i});
+        eigenMax(1,len) = max(eigen(:,len));
+        SigmaDet(i) = det(SigmaSens{i});
+        SigmaTotalNorm(i) = norm(SigmaSens{i});
+        % norm-2 for a symmetric matrix (see MatrixCookbook 3.1.5)
+        SigmaTotalNorm2(i) = max(abs(eig(SigmaSens{i}))); 
 
         % compute S_R_L = S_R_G x G_R_L
         S_R_L = G_R_S' * G_R_L;
@@ -41,6 +70,8 @@ for sIdx = 1: suit.properties.nrOfSensors
     end
     % matrix system
     B_pos_SL = A\b;
+    
+    Sigma_xgivena = inv(Sigma_xgivena_inv);
 
     sensor.origin = B_pos_SL;
     suit.sensors{sIdx}.position = sensor.origin;
